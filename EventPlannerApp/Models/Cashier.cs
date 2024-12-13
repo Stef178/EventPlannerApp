15d77e723bceaece78389a6e7e5b9127717e1a31
Stef Konings <2302771konings@zuyd.nl>
2024-12-13 13:34:27 +0100

    Reapply "Ticket afhandelen toegevoegd"
    This reverts commit 846db3c788120d5a7e37cd567035d0b3a455a83a.

--- EventPlannerApp/Models/Cashier.cs
+++ EventPlannerApp/Models/Cashier.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Ticket> Tickets { get; set; }
+        public ICollection<Ticket>? Tickets { get; set; }
     }
 
 }
